a9b1b02f7244aef2e5b2cd263ae36b63e185ae71
Saajith22 <mohammedsaajith93@gmail.com>
2022-08-03 16:36:52 +0400

    stats board

--- lib/Board.ts
+++ lib/Board.ts
@@ -7,6 +7,7 @@ type Stats = {
   points: {
     x: number;
     y: number;
+    value: string;
   }[];
 };
 
@@ -22,14 +23,31 @@ export default class Board {
     this.ctx.fillStyle = "white";
     this.ctx.fillRect(0, 0, width, height);
 
-    this.ctx.fillStyle = "black";
-    this.ctx.strokeStyle = "bold";
+    let gap = 20;
+
+    this.ctx.lineWidth = 10;
     this.ctx.beginPath();
-    this.ctx.moveTo(10, 0);
-    this.ctx.lineTo(10, height - 10);
-    this.ctx.lineTo(width - 10, height - 10);
+    this.ctx.moveTo(gap, gap);
+    this.ctx.lineTo(gap, height - gap);
+    this.ctx.lineTo(width - gap, height - gap);
     this.ctx.stroke();
     this.ctx.closePath();
+
+    this.ctx.strokeStyle = "red";
+    stats.points.forEach((p, i) => {
+      let prev = i < 1 ? { x: gap, y: height - gap } : stats.points[i - 1];
+      let { x, y } = p;
+
+      this.ctx.beginPath();
+      this.ctx.moveTo(prev.x, prev.y);
+      this.ctx.lineTo(x, y);
+      this.ctx.stroke();
+      this.ctx.closePath();
+
+      this.ctx.fillStyle = "red";
+      this.ctx.font = "20px Sans";
+      this.ctx.fillText(p.value, x - gap, y - gap);
+    });
   }
 
   getImage() {
